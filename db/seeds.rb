35768f266fd8da696d9ca3740e71ffd37f3760db
Turo Hakulinen <turo.hakulinen@gmail.com>
2017-02-14 11:55:26 +0000

    added task model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name:"code", description: "do exercise", done: "false")
+Task.create(name:"code more", description: "do exercise", done: "false")
+Task.create(name:"code more more", description: "do exercise", done: "false")
+Task.create(name:"code rest", description: "do exercise", done: "false")
